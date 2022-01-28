3f353a73f8ec9c9be2b06de48968c716ef3e2bb1
Nguyen Hoang Thien Phuc <phuc.nghoangthien@gmail.com>
2022-01-28 17:24:07 +0700

    Fix domain

--- components/display/Image.tsx
+++ components/display/Image.tsx
@@ -1,11 +1,10 @@
 import NextImage from 'next/image';
 
-// replace [yourprojectname] and [yourdomain.com] with your actual project name and (custom) domain
 const cloudflareImageLoader = ({ src, width, quality }: { src: string; width: number; quality: number }) => {
   if (!quality) {
     quality = 75;
   }
-  return `https://images.[yourprojectname].workers.dev?width=${width}&quality=${quality}&image=https://[yourdomain.com]${src}`;
+  return `https://images.mymetafarm.workers.dev?width=${width}&quality=${quality}&image=https://mymetafarm.pages.dev${src}`;
 };
 
 export default function Image(props: any) {
