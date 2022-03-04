5df856096dc6e2707269078f0f386a363b70e49f
Nguyen Hoang Thien Phuc <phuc.nghoangthien@gmail.com>
2022-03-04 14:24:39 +0700

    Open mkp

--- components/layouts/app/header/Header.tsx
+++ components/layouts/app/header/Header.tsx
@@ -23,8 +23,8 @@ const Header = () => {
         </Box>
         <ul className="w-full col-span-3 flex gap-x-2 justify-end items-baseline">
           <NavLink href="/" exact content="Home" />
-          <NavLink href="/dashboard/box" content="Dashboard" />
-          <NavLink disabled={isProduction} href="/marketplace/items" content="Marketplace" />
+          <NavLink disabled={isProduction} href="/dashboard/box" content="Dashboard" />
+          <NavLink href="/marketplace/items" content="Marketplace" />
           <NavLink href="/metaverse" content="Metaverse" />
           <NavLink disabled href="/document" content="Document" />
           <HeaderButtonUser />
