fb7eb99ca219eafc892d4fbbbc13ebdf7e943f89
Nguyen Hoang Thien Phuc <phuc.nghoangthien@gmail.com>
2022-03-09 09:39:25 +0700

    Fix box api

--- store/box/box.api.ts
+++ store/box/box.api.ts
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { clientBox, clientMarket } from 'utils/api';
+import { clientMarket } from 'utils/api';
 import { GetBoxesRequest, GetBoxHashMessageRequest } from './box.i';
 
-export const getBoxes = (params?: GetBoxesRequest) => clientBox.get(`/`, { params });
-export const getBox = (id: string) => clientMarket.get(`/${id}`);
+export const getBoxes = (params?: GetBoxesRequest) => clientMarket.get(`/boxes`, { params });
+export const getBox = (id: string) => clientMarket.get(`/boxes/${id}`);
 export const getBoxHashMessage = (id: string, params?: GetBoxHashMessageRequest) =>
-  clientMarket.get(`/${id}/hash-message`, { params });
-export const syncBoxes = () => clientMarket.post(`/data-sync`);
-export const createSale = (id: string) => clientMarket.post(`/${id}/sale`);
-export const delateSale = (id: string) => clientMarket.delete(`/${id}/sale`);
+  clientMarket.get(`/boxes/${id}/hash-message`, { params });
+export const syncBoxes = () => clientMarket.post(`/boxes/data-sync`);
+export const createSale = (id: string) => clientMarket.post(`/boxes/${id}/sale`);
+export const delateSale = (id: string) => clientMarket.delete(`/boxes/${id}/sale`);
 
 export const getBoxesThunk = createAsyncThunk('boxes/get', getBoxes);
