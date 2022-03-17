0a738a39cb90bb0df3a388a90ad33df2b9605e7a
Nguyen Hoang Thien Phuc <phuc.nghoangthien@gmail.com>
2022-03-17 21:00:50 +0700

    Fix prod warn

--- hooks/useVideoAutoPlayback.ts
+++ hooks/useVideoAutoPlayback.ts
@@ -6,14 +6,15 @@ const useVideoAutoPlayback = (options: any) => {
 
   const cb = (entries: any) => {
     const [entry] = entries;
-    if (entry.isIntersecting) videoRef.current?.play();
-    else videoRef.current?.pause();
+    if (entry.isIntersecting) (videoRef.current as any).play();
+    else (videoRef.current as any).pause();
   };
 
   useEffect(() => {
     const observer = new IntersectionObserver(cb, options);
     if (containerRef.current) observer.observe(containerRef.current);
     return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
       if (containerRef.current) observer.unobserve(containerRef.current);
     };
   }, [containerRef, options]);
