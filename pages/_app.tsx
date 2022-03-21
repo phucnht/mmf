b88578c1d97f52b65cc2ee7a12ee95ed0d0e5da7
Nguyen Hoang Thien Phuc <phuc.nghoangthien@gmail.com>
2022-03-21 16:17:34 +0700

    Add gg analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -50,6 +50,18 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           })(window,document,'script','dataLayer','GTM-P734JPJ');
         `}
       </Script>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
       <Head>
         <link rel="shortcut icon" href="/assets/logos/favicon.ico" />
       </Head>
