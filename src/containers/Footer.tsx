0c2a6fd31e3a60c0ecfa770a3a1070fc7d2cd79e
WindzLord <kk.outplayed@gmail.com>
2022-06-11 21:58:49 +0700

    Footer social

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -29,23 +29,43 @@ const Footer = () => {
               </div>
               <div className='flex gap-3'>
                 <TextField placeholder='Enter your email' InputProps={{ className: 'bg-white text-black' }} />
-                <Button>Subcribe</Button>
+                <Button>Subscribe</Button>
               </div>
             </Grid>
             <Grid item md={5.5} className='flex flex-col items-start gap-3'>
               <div className='font-black'>JOIN OUR COMMUNITY</div>
               <div className='flex flex-wrap gap-3'>
                 {[
-                  { url: '', icon: require('assets/icons/socials/facebook.svg').default.src },
-                  { url: '', icon: require('assets/icons/socials/instagram.svg').default.src },
-                  { url: '', icon: require('assets/icons/socials/twitter.svg').default.src },
-                  { url: '', icon: require('assets/icons/socials/discord.svg').default.src },
-                  { url: '', icon: require('assets/icons/socials/telegram.svg').default.src },
-                  { url: '', icon: require('assets/icons/socials/youtube.svg').default.src },
+                  {
+                    url: 'https://www.facebook.com/MyMetaFarmOfficial',
+                    icon: require('assets/icons/socials/facebook.svg').default.src,
+                  },
+                  {
+                    url: 'https://instagram.com/MyMetaFarmOfficial',
+                    icon: require('assets/icons/socials/instagram.svg').default.src,
+                  },
+                  {
+                    url: 'https://twitter.com/MyMetaFarm',
+                    icon: require('assets/icons/socials/twitter.svg').default.src,
+                  },
+                  {
+                    url: 'https://discord.gg/MyMetaFarm',
+                    icon: require('assets/icons/socials/discord.svg').default.src,
+                  },
+                  {
+                    url: 'https://t.me/MyMetaFarm',
+                    icon: require('assets/icons/socials/telegram.svg').default.src,
+                  },
+                  {
+                    url: 'https://www.youtube.com/channel/UCQ_NUT5gSaYfEDq61YbhDlA',
+                    icon: require('assets/icons/socials/youtube.svg').default.src,
+                  },
                 ].map((item, index) => (
-                  <Button key={index} className='bg-green-500 w-[48px] px-0'>
-                    <img src={item.icon} />
-                  </Button>
+                  <a key={index} href={item.url} target='_blank' rel='noreferrer'>
+                    <Button className='bg-green-500 hover:opacity-80 w-[48px] h-[48px] px-0'>
+                      <img src={item.icon} />
+                    </Button>
+                  </a>
                 ))}
               </div>
             </Grid>
